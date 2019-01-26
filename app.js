1fc86232de100ca7c75b23dc389b9247dc5ae74c
unihaoke <1261654234@qq.com>
2019-01-27 00:20:21 +0800

    第一次提交

---
+++ app.js
@@ -0,0 +1,112 @@
+//app.js
+App({
+  onLaunch: function () {
+    // 展示本地存储能力
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
+  },
+  //第一种状态的底部
+  editTabBar: function () {
+    var _curPageArr = getCurrentPages();
+    var _curPage = _curPageArr[_curPageArr.length - 1];
+    var _pagePath = _curPage.__route__;
+    if (_pagePath.indexOf('/') != 0) {
+      _pagePath = '/' + _pagePath;
+    }
+    var tabBar = this.globalData.tabBar;
+    for (var i = 0; i < tabBar.list.length; i++) {
+      tabBar.list[i].active = false;
+      if (tabBar.list[i].pagePath == _pagePath) {
+        tabBar.list[i].active = true;//根据页面地址设置当前页面状态  
+      }
+    }
+    _curPage.setData({
+      tabBar: tabBar
+    });
+  },
+  getUserInfo: function (cb) {
+    var that = this
+    if (this.globalData.userInfo) {
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    } else {
+      //调用登录接口
+      wx.login({
+        success: function () {
+          wx.getUserInfo({
+            success: function (res) {
+              that.globalData.userInfo = res.userInfo
+              typeof cb == "function" && cb(that.globalData.userInfo)
+            }
+          })
+        }
+      })
+    }
+  },
+  globalData: {
+    userInfo: null,
+    tabBar: {
+      "color": "#9E9E9E",
+      "selectedColor": "#f00",
+      "backgroundColor": "#fff",
+      "borderStyle": "#ccc",
+      "list": [
+        {
+          "pagePath": "/pages/team/team_index",
+          "iconPath": "/pages/imge/task_list.png",
+          "selectedIconPath": "/pages/imge/task_list_select.png",
+          "text": "任务",
+          "selectedColor": "#4EDF80",
+          "clas": "menu-item",
+          active: true
+        },
+        {
+          "pagePath": "/pages/team/team_message",
+          "text": "消息",
+          "iconPath": "/pages/imge/team_message.png",
+          "selectedIconPath": "/pages/imge/team_message_select.png",
+          "selectedColor": "#4EDF80",
+          "clas": "menu-item",
+          active: false
+        },
+        {
+          "pagePath": "/pages/team/team_member",
+          "text": "成员",
+          "iconPath": "/pages/imge/member_list.png",
+          "selectedIconPath": "/pages/imge/member_list_select.png",
+          "selectedColor": "#4EDF80",
+          "clas": "menu-item",
+          active: false
+        }
+      ],
+      "position": "bottom"
+    }
+  }
+})
